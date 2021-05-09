e5a4f9c14ddc642b2bddaca4554df8a0ba2987e3
PatHealy <pwh5@pitt.edu>
2021-05-09 00:56:36 -0400

    Added tetris example

--- matrix-examples/python/samples/tetris_grid.py
+++ matrix-examples/python/samples/tetris_grid.py
@@ -8,17 +8,20 @@ class TetrisGrid(SampleBase):
     def run(self):
         offset_canvas = self.matrix.CreateFrameCanvas()
         while True:
-            for x in range(0, self.matrix.width):
-                offset_canvas.SetPixel(x, x, 255, 255, 255)
-                offset_canvas.SetPixel(offset_canvas.height - 1 - x, x, 255, 0, 255)
+            grid = []
+            for x in range(16):
+                row = []
+                for y in range(16):
+                    if x%2 == y%2:
+                        row.append((0,0,0))
+                    else:
+                        row.append((255,255,255))
+                grid.append(row)
 
-            for x in range(0, offset_canvas.width):
-                offset_canvas.SetPixel(x, 0, 255, 0, 0)
-                offset_canvas.SetPixel(x, offset_canvas.height - 1, 255, 255, 0)
-
-            for y in range(0, offset_canvas.height):
-                offset_canvas.SetPixel(0, y, 0, 0, 255)
-                offset_canvas.SetPixel(offset_canvas.width - 1, y, 0, 255, 0)
+            for x in range(self.matrix.width):
+                for y in range(self.matrix.height):
+                    c = grid[((int)x)/4][((int)y)/4]
+                    offset_canvas.SetPixel(x, x, 255, 255, 255)
             offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
 
