7ee3c3f65e85f1fefcc701ea8aa63a65e2b4ce03
PatHealy <pwh5@pitt.edu>
2021-05-09 01:34:12 -0400

    led_tetris test

--- led_tetris.py
+++ led_tetris.py
@@ -1,6 +1,5 @@
-import sys
-from sty import fg, bg, ef, rs, RgbFg
 from entities import Tetris
+from matrix_examples.python.samples.samplebase import SampleBase
 
 class _Getch:
 	"""Gets a single character from standard input.  Does not echo to the
@@ -36,34 +35,46 @@ class _GetchWindows:
 		import msvcrt
 		return msvcrt.getch()
 
-class TerminalTetrisRunner:
+class TetrisGrid(SampleBase):
+	def __init__(self, *args, **kwargs):
+		super(TetrisGrid, self).__init__(*args, **kwargs)
+		self.tet_grid = []
+		for x in range(16):
+			row = []
+			for y in range(16):
+				if x%2 == y%2:
+					row.append((0,0,0))
+				else:
+					row.append((255,255,255))
+			self.tet_grid.append(row)
 
+	def set_grid(self, gr):
+		self.tet_grid = gr
+
+	def run(self):
+		offset_canvas = self.matrix.CreateFrameCanvas()
+		while True:
+			for x in range(self.matrix.width):
+				for y in range(self.matrix.height):
+					c = self.tet_grid[(int(x))/4][(int(y))/4]
+					offset_canvas.SetPixel(x, y, c[0], c[1], c[2])
+			offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+
+
+class GridTetrisRunner:
 	def __init__(self):
+		self.led_grid = TetrisGrid()
 		self.width = 16
 		self.height = 16
 		self.game = Tetris(self.width, self.height)
 
 	def new_game(self):
 		self.game = Tetris(self.width, self.height)
-		self.erase_board()
-
-	def delete_last_lines(self,n):
-		CURSOR_UP_ONE = '\033[F' #'\x1b[1A'
-		ERASE_LINE = '\033[K' #'\x1b[2K'
-		for _ in range(n):
-			sys.stdout.write(CURSOR_UP_ONE)
-			sys.stdout.write(ERASE_LINE)
-
-	def erase_board(self):
-		self.delete_last_lines(self.height + 3)
+		self.print_board()
 
 	def print_board(self):
 		data = self.game.get_board()
-		for i in range(len(data)):
-			line = ''
-			for cell in data[len(data) - 1 - i]:
-				line = line + fg(cell[0], cell[1], cell[2]) + '@' + fg.rs
-			print(line)
+		self.led_grid.set_grid(data)
 
 	def get_input(self):
 		char = _Getch().__call__()
@@ -90,21 +101,11 @@ class TerminalTetrisRunner:
 			while not self.game.is_failed():
 				self.print_board()
 				self.get_input()
-				self.erase_board()
+				#self.erase_board()
 			self.print_board()
 			self.get_input()
 			self.new_game()
 
-
-
-
-# Main function
-# if __name__ == "__main__":
-#     tetris_grid = TetrisGrid()
-#     if (not tetris_grid.process()):
-#         tetris_grid.print_help()
-
-
 if __name__ == '__main__':
-	runner = TerminalTetrisRunner()
+	runner = GridTetrisRunner()
 	runner.play()
