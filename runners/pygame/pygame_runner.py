9b2592f48e04cebc8cf544fbb2bf6ded75230125
Pat Healy <pwh5@pitt.edu>
2022-04-20 02:26:41 -0400

    Fixed input scheme to be more responsive.

--- runners/pygame/pygame_runner.py
+++ runners/pygame/pygame_runner.py
@@ -22,37 +22,47 @@ class PygameTetrisRunner(TetrisRunner):
 		pygame.display.flip()
 
 	def get_input(self):
+		success = False
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				exit()
-
-		success = False
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_w]:
-			self.tick = 0
-			success = self.game.snap_piece()
-		elif keys[pygame.K_a]:
-			self.tick = 0
-			success = self.game.move_piece('left')
-		elif keys[pygame.K_s]:
-			self.tick = 0
-			success = self.game.move_piece('down')
-		elif keys[pygame.K_d]:
-			self.tick = 0
-			success = self.game.move_piece('right')
-		elif keys[pygame.K_r]:
-			self.tick = 0
-			success = self.game.rotate_piece()
-		elif keys[pygame.K_x]:
-			pygame.quit()
-			exit()
-		else:
-			self.tick = self.tick + 1
-			if self.tick > 5:
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_a:
+					success = self.game.move_piece('left')
+					self.tick = 0
+				elif event.key == pygame.K_w:
+					self.tick = 0
+					success = self.game.snap_piece()
+				elif event.key == pygame.K_s:
+					self.tick = 0
+					success = self.game.move_piece('down')
+				elif event.key == pygame.K_d:
+					self.tick = 0
+					success = self.game.move_piece('right')
+				elif event.key == pygame.K_r:
+					self.tick = 0
+					success = self.game.rotate_piece()
+				elif event.key == pygame.K_x:
+					pygame.quit()
+					exit()
+		self.tick = self.tick + 1
+		if self.tick > 10:
+			keys = pygame.key.get_pressed()
+			if keys[pygame.K_a]:
+				success = self.game.move_piece('left')
 				self.tick = 0
+			elif keys[pygame.K_d]:
+				success = self.game.move_piece('right')
+				self.tick = 0
+			elif keys[pygame.K_s]:
 				success = self.game.move_piece('down')
-		self.clock.tick(7)
+				self.tick = 0
+		if self.tick > 60:
+			success = self.game.move_piece('down')
+			self.tick = 0
+
+		self.clock.tick(40)
 		return success
 
 	def play(self):
